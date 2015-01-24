5e69eb913f43154a705b1811a6f0bb8b9c2c4db8
Arkady Galyash <my_another@mail.ru>
2015-01-24 23:59:14 +0300

    database to store oauth keys

--- src/storage/gorlim_storage.go
+++ src/storage/gorlim_storage.go
@@ -2,10 +2,13 @@ package storage
 
 import (
 	"database/sql"
+	"errors"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 type Storage interface {
+	GetGithubAuth(user string) (string, error)
+	SaveGithubAuth(user, auth string) error
 	GetRepos(needle string) ([]*Repo, error)
 	AddRepo(myType, origin, target string) error
 }
@@ -19,6 +22,35 @@ type Repo struct {
 type repoImpl struct {
 	connection *sql.DB
 	noRepos    []*Repo
+}
+
+func (r repoImpl) SaveGithubAuth(user, auth string) error {
+	stmt, err := r.connection.Prepare("insert or replace into github_auth (login, auth) values(?, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Query(user, auth)
+	return err
+}
+
+func (r repoImpl) GetGithubAuth(user string) (string, error) {
+	stmt, err := r.connection.Prepare("select auth from github_auth where login equals ?")
+	if err != nil {
+		return "", err
+	}
+	defer stmt.Close()
+	rows, err := stmt.Query(user)
+	if err != nil {
+		return "", err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var auth string
+		rows.Scan(&auth)
+		return auth, nil
+	}
+	return "", errors.New("No auth found for " + user)
 }
 
 func (r repoImpl) AddRepo(myType, origin, target string) error {
@@ -66,6 +98,11 @@ func Create(filename string) (*Storage, error) {
 				origin text not null, 
 				target text not null
 			) ;
+			create table if not exists github_auth
+			(
+				login text primary key,
+				auth text
+			);
 			`
 	_, err = db.Exec(sqlStmt)
 	if err != nil {
