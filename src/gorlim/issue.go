2ae8e1ec1bf7c7f7013b527762b2bde59ddc8308
QuietBird <sokolova.irina0@gmail.com>
2015-01-25 11:42:46 +0300

    creator added

--- src/gorlim/issue.go
+++ src/gorlim/issue.go
@@ -1,14 +1,13 @@
 package gorlim
 
 type Issue struct {
-  Id int
-  Opened bool
-  Creator string
-  Assignee string
-  Milestone string
-  Title string
-  Description string
-  Labels []string
-  Comments []string
+	Id          int
+	Opened      bool
+	Creator     string
+	Assignee    string
+	Milestone   string
+	Title       string
+	Description string
+	Labels      []string
+	Comments    []string
 }
-
